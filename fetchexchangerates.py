502b08a6664436bd543d8a68e41f64147d18015c
Mike Stucka <stucka@github>
2015-12-17 08:47:45 -0500

    Swapped insane data typing for sane type

--- fetchexchangerates.py
+++ fetchexchangerates.py
@@ -13,37 +13,36 @@ r = requests.get(url)
 json = r.json()
 rates = json["rates"]
 
-codes = OrderedDict([
-    ('Euro*', ['EUR', '']),
-    ('Argentina', ['ARS', 'pesos']),
-    ('Australia', ['AUD', 'dollars']),
-    ('Brazil', ['BRL', 'reals']),
-    ('Britain', ['GBP', 'pounds']),
-    ('Canada', ['CAD', 'dollars']),
-    ('China', ['CNY', 'yuan']),
-    ('Denmark', ['DKK', 'kroner']),
-    ('Egypt', ['EGP', 'pounds']),
-    ('Hong Kong', ['HKD', 'dollars']),
-    ('India', ['INR', 'rupees']),
-    ('Israel', ['ILS', 'shekels']),
-    ('Jamaica', ['JMD', 'dollars']),
-    ('Japan', ['JPY', 'yen']),
-    ('Kenya', ['KES', 'shillings']),
-    ('Mexico', ['MXN', 'pesos']),
-    ('Norway', ['NOK', 'kroner']),
-    ('S. Africa', ['ZAR', 'rand']),
-    ('Sweden', ['SEK', 'kronor']),
-    ('Switzerland', ['CHF', 'francs']),
-    ('Turkey', ['TRY', 'liras'])
-])
+currencies = (
+    ('Euro*', 'EUR', ''),
+    ('Argentina', 'ARS', 'pesos'),
+    ('Australia', 'AUD', 'dollars'),
+    ('Brazil', 'BRL', 'reals'),
+    ('Britain', 'GBP', 'pounds'),
+    ('Canada', 'CAD', 'dollars'),
+    ('China', 'CNY', 'yuan'),
+    ('Denmark', 'DKK', 'kroner'),
+    ('Egypt', 'EGP', 'pounds'),
+    ('Hong Kong', 'HKD', 'dollars'),
+    ('India', 'INR', 'rupees'),
+    ('Israel', 'ILS', 'shekels'),
+    ('Jamaica', 'JMD', 'dollars'),
+    ('Japan', 'JPY', 'yen'),
+    ('Kenya', 'KES', 'shillings'),
+    ('Mexico', 'MXN', 'pesos'),
+    ('Norway', 'NOK', 'kroner'),
+    ('S. Africa', 'ZAR', 'rand'),
+    ('Sweden', 'SEK', 'kronor'),
+    ('Switzerland', 'CHF', 'francs'),
+    ('Turkey', 'TRY', 'liras')
+)
 
-#print(rates)
 print("\r\n")
 print("$1 EQUALS ...\r\n")
 
-
-for country in codes:
-    print(country + "\t" + "{0:.2f}".format(rates[codes[country][0]]) + "\t" + codes[country][1] + "\r\n")
+for currency in currencies:
+    country, abbreviation, denomination = currency
+    print(country + "\t" + "{0:.2f}".format(rates[abbreviation]) + "\t" + denomination + "\r\n")
     
 print("\r\n")
 print("* Euro nations are Austria, Belgium, Cyprus, Estonia, Finland, France, Germany, Greece, Ireland, Italy, Latvia, Lithuania, Luxembourg, Malta, the Netherlands, Portugal, Slovakia, Slovenia and Spain.\r\n")
